7053d813370f6d4f897a7b0593d79c5d31d2a6e2
max.s <max.s@ilyon.net>
2019-12-24 20:18:01 +0200

    add simple scripts

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+using Zenject;
+
+public class UiController : MonoBehaviour {
+    [SerializeField] private GameObject _mainMenu;
+    [SerializeField] private GameObject _gameMenu;
+
+    [Inject] private GameController _gameController;
+
+    private void Awake()
+    {
+        Debug.Log(_gameController.name);
+    }
+
+    private void Update()
+    {
+    }
+
+#if UNITY_EDITOR
+    private const string MainMenuName = "main menu";
+    private const string ButtonRestart = "button - restart";
+
+    private const string GameMenuName = "game menu";
+    private const string ButtonPlay = "button - play";
+    private const string ButtonExit = "button - exit";
+
+    private void OnValidate()
+    {
+        Transform mainMenuTransform = transform.Find(MainMenuName);
+        if (mainMenuTransform == null) {
+            Debug.LogError("UI should have view with name \"" + MainMenuName + "\"");
+        }
+        else {
+            _mainMenu = mainMenuTransform.gameObject;
+
+            AttachMethodToButton(_mainMenu, ButtonPlay, OnPlayClicked);
+            AttachMethodToButton(_mainMenu, ButtonExit, OnExitClicked);
+        }
+
+        Transform gameMenuTransform = transform.Find(GameMenuName);
+
+        if (gameMenuTransform == null) {
+            Debug.LogError("UI should have child with name \"" + GameMenuName + "\"");
+        }
+        else {
+            _gameMenu = gameMenuTransform.gameObject;
+            AttachMethodToButton(_gameMenu, ButtonRestart, OnRestartClicked);
+        }
+    }
+
+    private void AttachMethodToButton(GameObject go, string buttonName, UnityAction method)
+    {
+        Transform restartButtonTransform = go.transform.Find(buttonName);
+        if (restartButtonTransform == null) {
+            Debug.LogError("main menu should have button with name \"" + buttonName + "\"");
+        }
+        else {
+            Button exitButton = restartButtonTransform.gameObject.GetComponent<Button>();
+            exitButton.onClick.AddListener(method);
+        }
+    }
+#endif
+
+    public void ShowMainMenu()
+    {
+        _mainMenu.SetActive(true);
+    }
+
+    public void HideMainMenu()
+    {
+        _mainMenu.SetActive(false);
+    }
+
+    public void ShowGameMenu()
+    {
+        _gameMenu.SetActive(true);
+    }
+
+    public void HideGameMenu()
+    {
+        _gameMenu.SetActive(false);
+    }
+
+    public void OnPlayClicked()
+    {
+        Debug.Log("Play");
+    }
+
+    public void OnExitClicked()
+    {
+        Debug.Log("Exit");
+    }
+
+    public void OnRestartClicked()
+    {
+        Debug.Log("Restart");
+    }
+}
